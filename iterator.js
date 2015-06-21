1f02f8b927f8ce25a67611fa60f9e569473e0859
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-06-21 14:06:03 -0400

    iterator

--- iterator.js
+++ iterator.js
@@ -1,13 +1,28 @@
 'use strict';
 
-function Iterator(collection) {
+function Iterator(input) {
+	return typeof input === 'function' ? FnIterator(input) : ArrayIterator(input);
+}
+
+function ArrayIterator(collection) {
 	var pointer = 0, count = collection.length;
 
 	return {
 		next: function() {
 			return {
 				value: collection[pointer++],
-				done: pointer === count
+				done: pointer >= count
+			};
+		}
+	};
+}
+
+function FnIterator(fn) {
+	return {
+		next: function(a) {
+			return {
+				value: fn(a),
+				done: false
 			};
 		}
 	};
