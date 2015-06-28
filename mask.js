1dc399e1d8b30ab214b4fc419f73f85bb1b38c9d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-06-28 17:09:17 -0500

    mask structure

---
+++ mask.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function Mask(keys, hasSum) {
+	var acc = emptyObject();
+
+	if (hasSum) {
+		acc.ALL = Math.pow(2, keys.length) - 1;
+	}
+
+	return Object.freeze(keys.reduce(function(acc, key, index) {
+		acc[key] = Math.pow(2, index);
+		return acc;
+	}, acc));
+}
+
+exports.Mask = Mask;
