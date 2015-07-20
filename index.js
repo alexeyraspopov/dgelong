ac12971e02716dcbd607984f6567b5d014248e12
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-07-20 12:59:51 -0400

    fix iterator link

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 exports.Maybe = require('./maybe');
 exports.Either = require('./either');
 exports.Future = require('./future');
-exports.Iterator = require('./iterator');
 exports.Observable = require('./observable');
