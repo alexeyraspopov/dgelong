c7ed70705fb87e0631bb186bde45fa132d753d01
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-08-08 13:58:36 -0400

    lazy future POC

---
+++ future-lazy-test.js
@@ -0,0 +1,22 @@
+function Future(producer) {
+	return {
+		bind: function(right) {
+			return Future(function(resolve) {
+				producer(function(value) {
+					resolve(right(value));
+				})
+			});
+		},
+		fork: function(fn) {
+			return producer(fn);
+		}
+	};
+}
+
+var a = Future(function(resolve) {
+	setTimeout(resolve, 1000, 13);
+});
+
+a.fork(function(v) {
+	console.log(v)
+});
