f9f0c20b0e8fcce67dee38fbda6ed7657e76f5dd
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-08-12 08:07:27 +0300

    lazy futures ftw

--- future.js
+++ future.js
@@ -1,72 +1,31 @@
-var Monad = require('./monad');
-
-function run(tasks, value) {
-	while (tasks.length) {
-		setTimeout(tasks.shift(), 0, value);
-	}
-}
-
-function pendingValue(producer) {
-	var isPending = true, value, pending = [];
-
-	// TODO: ensure that only one way will be invoked and only once
-	producer(function(result) {
-		isPending = false;
-		value = result;
-
-		run(pending, value);
-	}, function(error) {
-		// TODO: implement left part
-	});
-
-	return function(next) {
-		if (isPending) {
-			pending.push(next);
-		} else {
-			run([next], value);
-		}
-	};
-}
-
-function flatten(value, right, left) {
-	if (Monad.isMonad(value)) {
-		return value.bind(right, left);
-	} else {
-		return right(value);
-	}
-}
-
-function bindFuture(next, right, left) {
-	return Future(function(resolve, reject) {
-		next(function(value) {
-			flatten(right(value), resolve);
-		}, reject);
-	});
-}
+var compose = require('./compose');
 
 function Future(producer) {
-	var value = pendingValue(producer);
-
 	return {
 		isMonad: true,
 		bind: function(right, left) {
-			return bindFuture(value, right, left);
+			return Future(function(resolve, reject) {
+				producer(compose(resolve, right), compose(reject, left));
+			});
+		},
+		subscribe: function(fn) {
+			return producer(fn);
 		}
 	};
 }
 
-function Resolve(value) {
-	return Future(function(resolve) {
-		resolve(value);
-	});
+function Rejected() {
+	// TODO: implement me...
 }
 
-function Reject(error) {
-	return Future(function(resolve, reject) {
-		reject(error);
-	});
+function Resolved() {
+	// TODO: implement me...
 }
 
-exports.Resolve = Resolve;
-exports.Reject = Reject;
-exports.Future = Future;
+var a = Future(function(resolve) {
+	setTimeout(resolve, 1000, 13);
+});
+
+a.subscribe(function(v) {
+	console.log(v)
+});
