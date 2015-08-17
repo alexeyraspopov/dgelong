0ac6fca285302452912cc2a644a4f016d7c6996d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-08-17 11:16:16 +0300

    todos

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// TODO: check how stacktrace looks like
+// TODO: play with Rx
+// TODO: rules for resolve, reject, onNext, onError, onCompleted
+
 exports.Maybe = require('./maybe');
 exports.Either = require('./either');
 exports.Future = require('./future');
