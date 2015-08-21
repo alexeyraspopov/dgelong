426dc1cebd31861da0b1d400673de0759f5f4031
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-08-21 07:52:47 +0300

    unnecessary

--- operators.js
+++ operators.js
@@ -30,14 +30,6 @@ exports.unique = function unique(observable) {
 	});
 };
 
-exports.lift = function lift(fn, a, b) {
-	return a.bind(function(valueA) {
-		return b.bind(function(valueB) {
-			return fn(a, b);
-		});
-	});
-};
-
 exports.async = function async(generator) {
 	return Future(function(resolve, reject) {
 		var iterator = generator();
