f617b0c7a5ecce14aa86f6a87eb8cf5d57b29a1a
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-08-24 18:22:36 +0300

    export all the things

--- index.js
+++ index.js
@@ -6,5 +6,22 @@ import Maybe, {Just, Nothing} from "./maybe";
 import Either, {Success, Failure} from "./either";
 import Future, {Resolve, Reject} from "./future";
 import Observable from "./observable";
+import * as Monad from "./lib/monad";
+import compose from "./lib/compose";
+import onetime from "./lib/onetime";
+import operators from "/lib/operators";
 
-export default { Maybe, Just, Nothing, Either, Success, Failure, Future, Resolve, Reject, Observable };
+export default {
+	Maybe,
+	Just,
+	Nothing,
+	Either,
+	Success,
+	Failure,
+	Future,
+	Resolve,
+	Reject,
+	Observable,
+	Monad,
+	utils: { compose, onetime, operators }
+};
