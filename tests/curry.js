adddca2b42d9cc4e7aeb19b7dc7939c13da6820c
Alexey Raspopov <alexey.raspopov@datarobot.com>
2015-09-06 19:10:31 +0300

    used arrow functions

--- tests/curry.js
+++ tests/curry.js
@@ -2,16 +2,12 @@ import curry from "../lib/curry";
 import expect from "expect.js";
 
 describe("curry", function() {
-	function add(a, b, c) {
-		return a + b + c;
-	}
-
 	it("should curry", function() {
-		var cAdd = curry(add);
+		var add = curry((a, b, c) => a + b + c);
 
-		expect(cAdd(1, 2, 3)).to.be(6);
-		expect(cAdd(1)(2, 3)).to.be(6);
-		expect(cAdd(1, 2)(3)).to.be(6);
-		expect(cAdd(1)(2)(3)).to.be(6);
+		expect(add(1, 2, 3)).to.be(6);
+		expect(add(1)(2, 3)).to.be(6);
+		expect(add(1, 2)(3)).to.be(6);
+		expect(add(1)(2)(3)).to.be(6);
 	});
 });
